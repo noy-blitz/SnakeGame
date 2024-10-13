e4a6002db1203b2dc559a145be0710c043a10b9d
Noy Blitzblau <nblitzbl@campus.haifa.ac.il>
2024-10-13 08:49:01 +0300

    first commit

---
+++ SnakeQuiz/Model/SnakeModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace SnakeQuiz.Model
+{
+    public class SnakeModel
+    {
+        public List<Point> SnakeParts { get; set; }
+        public Point FoodPosition { get; set; }
+        public int Direction { get; set; } // 0: Up, 1: Right, 2: Down, 3: Left
+        public bool IsGameOver { get; set; }
+
+        public SnakeModel()
+        {
+            SnakeParts = new List<Point> { new Point(5, 5) }; // Initial position
+            GenerateFood();
+            Direction = 1; // Initial direction (Right)
+            IsGameOver = false;
+        }
+
+        public void Move()
+        {
+            if (IsGameOver) return;
+
+            // Move snake logic here
+        }
+
+        public void ChangeDirection(int newDirection)
+        {
+            // Change direction logic here
+        }
+
+        public void GenerateFood()
+        {
+            // Food generation logic here
+        }
+    }
+}
---
+++ SnakeQuiz/ViewModel/SnakeViewModel.cs
@@ -0,0 +1,52 @@
+﻿using SnakeQuiz.Commands;
+using SnakeQuiz.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+
+namespace SnakeQuiz.ViewModel
+{
+    public class SnakeViewModel : INotifyPropertyChanged
+    {
+        private SnakeModel _snakeModel;
+        public ICommand MoveCommand { get; set; }
+        public ICommand ChangeDirectionCommand { get; set; }
+
+        public SnakeViewModel()
+        {
+            _snakeModel = new SnakeModel();
+            MoveCommand = new RelayCommand(o => Move());
+            ChangeDirectionCommand = new RelayCommand(o => ChangeDirection(o));
+        }
+
+        public void Move()
+        {
+            _snakeModel.Move();
+            OnPropertyChanged(nameof(SnakeParts));
+            OnPropertyChanged(nameof(FoodPosition));
+            OnPropertyChanged(nameof(IsGameOver));
+        }
+
+        public void ChangeDirection(object direction)
+        {
+            _snakeModel.ChangeDirection((int)direction);
+        }
+
+        public List<Point> SnakeParts => _snakeModel.SnakeParts;
+        public Point FoodPosition => _snakeModel.FoodPosition;
+        public bool IsGameOver => _snakeModel.IsGameOver;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
